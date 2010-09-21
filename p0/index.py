a6613f58f83c3b5a14a6b39da984681233fc31fd
lionelmena@gmail.com <lionelmena@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-09-21 18:52:34 +0000

    Probando keywords

--- p0/index.py
+++ p0/index.py
@@ -1,3 +1,11 @@
+/*
+        $Id$
+Description: Programa que crea un indice de palabras para un texto.
+    $Author$ Lionel Aster Mena Garcia Alejandro Samarin
+      $Date$
+  $Revision$
+*/
+
 print 'esto es una prueba'
 for a in range(10):
    print 'compiladores'
