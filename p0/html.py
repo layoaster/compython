3fcbae1aad276b31d80586723ab396930cb11296
alejandrosamarin <alejandrosamarin@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-09-27 22:04:36 +0000

    Anyadiendo modulo de generacion de html

---
+++ p0/html.py
@@ -0,0 +1,32 @@
+def head(title = "Untitled"):
+	"""Cabeceras de html"""
+	return '''
+<html>
+<head>
+<title>''' + title + '''</title>
+</head>
+
+<body>\n\n'''
+
+def linkToGoogle(text):
+	"""Enlaza el texto a Google"""
+	return "<a href='http://www.google.es/search?q=" + text + "'>" + text + "</a>"
+
+def writeDictionary(d):
+	table = "<table border=0>\n"
+	sorted = d.keys()
+	sorted.sort()
+	for word in sorted:
+		table = table + "<tr>\n<td>" + linkToGoogle(str(word)) 
+		table = table + "&nbsp;&nbsp;&nbsp;</td>\n" + "<td>"
+		for num in d[word]:
+			table = table + str(num) + ", "
+		table = table[:-2]
+		table = table + "</td>\n</tr>\n"
+	return table + "</table>\n"
+
+def end():
+	"""Final de html"""
+	return '''
+</body>
+</html>'''
