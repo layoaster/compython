90f3a3989ffa57db812afa23ddfca79cf6ef2568
lionelmena@gmail.com <lionelmena@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-01 13:42:48 +0000

    Modulo del Analizador Lexico

---
+++ p1/lexan.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+        $Id$
+Description: Analizador Lexico para Pascal-.
+    $Author$ Lionel Aster Mena Garcia, Alejandro Samarin Perez, Sergio Armas Perez
+      $Date$
+  $Revision$
+"""
+
+import sys
+import string
