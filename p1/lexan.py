f65fcce24f9b83c96c362a7ae5f49dc0a557a539
lionelmena@gmail.com <lionelmena@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-01 17:12:54 +0000

    Esqueleto de la clase LexAn

--- p1/lexan.py
+++ p1/lexan.py
@@ -11,3 +11,29 @@ Description: Analizador Lexico para Pascal-.
 
 import sys
 import string
+
+class LexAn:
+    """Clase General de Analizador Lexico"""
+    __fin
+    __nline
+    __ncol
+    __line
+
+    def __init__(self):
+    """
+    """
+        self.__nline = 1
+        self.__ncol = 1
+        self.__line = []
+
+    def openFile(self, fin):
+        error = False
+        try:
+            self.__fin = open(fin, "rU")
+        except IOError:
+            error = True
+        return error
+
+    def yyLex():
+
+
