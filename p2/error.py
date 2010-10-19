831130bb8510606470a0b55305d8386ae50ef9e5
alejandrosamarin@gmail.com <alejandrosamarin@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-19 10:00:16 +0000

    Tocando error.py

--- p2/error.py
+++ p2/error.py
@@ -1,14 +1,18 @@
-class WrapErr():
+"""class WrapErr():
     # Lex errors
     UNKNOWN_CHAR = 0
     INT_OVERFLOW = 1
     UNCLOSED_COM = 2
-
+"""
 class Error(Exception):
     def __init__(self):
         pass
 
 class LexicalError(Error):
+    UNKNOWN_CHAR = 0
+    INT_OVERFLOW = 1
+    UNCLOSED_COM = 2
+    
     _errStrings = ("Invalid character",
                    "Integer overflow",
                    "Unclosed comment")
@@ -21,3 +25,9 @@ class LexicalError(Error):
 
     def _printError(self):
         print "\n[LEX ERROR] ", self._errStrings[self.err],
+
+
+class SyntacticalError(Error):
+    UNEXPECTED_SYM = 0
+
+    _errStrings = ("Unexpected symbol")
