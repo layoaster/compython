aeb6ae7519bb1d44b1134928414fe6a60496543a
alejandrosamarin@gmail.com <alejandrosamarin@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-20 16:27:19 +0000

    finally

--- p2/pmc.py
+++ p2/pmc.py
@@ -25,7 +25,7 @@ def webTree(tree):
 if __name__ == '__main__':
     # Especificacion del parseado de argumentos por linea de comandos
     parser = argparse.ArgumentParser(description='Realiza el Analisis Sintactico de ficheros de codigo fuente en Pascal-')
-    parser.add_argument('fin', metavar='fich_texto.pas', type=str, action='store', help='fichero de codigo fuente')
+    parser.add_argument('fin', metavar='fich_texto.p', type=str, action='store', help='fichero de codigo fuente')
     parser.add_argument('-t', metavar='fich_tree.html', type=str, dest="tree", help='genera fichero html con el AAS del codigo fuente')
 
     # Parametros insuficientes -> mostrar por pantalla el modo de uso
@@ -38,14 +38,15 @@ if __name__ == '__main__':
         try:
             parser.start(args.fin)
             print Colors.OKGREEN + "All OK" + Colors.ENDC
-            if args.tree:
-                webTree(args.tree)
-                print Colors.OKBLUE + "[INFO]" + Colors.ENDC + " WebAST written to '" + args.tree + "'"
         except Error as e:
             e.printError();
             exit(1)
         except IOError as e:
             print Colors.WARNING + e.filename + Colors.FAIL + " [I/O ERROR] " + Colors.ENDC + e.strerror
             exit(2)
+        finally:
+            if args.tree:
+                webTree(args.tree)
+                print "\n" + Colors.OKBLUE + "[INFO]" + Colors.ENDC + " WebAST written to '" + args.tree + "'"
 
 
