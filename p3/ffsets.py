db42a110373ed6c17bde47661cf75674eb7edf02
lionelmena@gmail.com <lionelmena@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-25 09:07:43 +0000

    mejorando/corrigiendo la clase ffsets

--- p3/ffsets.py
+++ p3/ffsets.py
@@ -16,130 +16,126 @@ class FFSets:
     def __init__(self):
         self._ffs = {}
 
-        self._ffs["program"]                = [[WrapTk.PROGRAM,],
-                                               [WrapTk.ENDTEXT,]]
+        self._ffs["program"]                = ([WrapTk.PROGRAM],
+                                               [WrapTk.ENDTEXT])
 
-        self._ffs["blockBody"]              = [[WrapTk.CONST, WrapTk.TYPE, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN],
-                                               [WrapTk.PERIOD, WrapTk.SEMICOLON]]
+        self._ffs["blockBody"]              = ([WrapTk.CONST, WrapTk.TYPE, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN],
+                                               [WrapTk.PERIOD, WrapTk.SEMICOLON])
 
-        self._ffs["constantDefinitionPart"] = [[WrapTk.CONST],
-                                               [WrapTk.TYPE, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN]]
+        self._ffs["constantDefinitionPart"] = ([WrapTk.CONST],
+                                               [WrapTk.TYPE, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN])
 
-        self._ffs["constantDefinition"]     = [[WrapTk.ID],
-                                               [WrapTk.ID, WrapTk.TYPE, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN]]
+        self._ffs["constantDefinition"]     = ([WrapTk.ID],
+                                               [WrapTk.ID, WrapTk.TYPE, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN])
 
 
-        self._ffs["typeDefinitionPart"]     = [[WrapTk.TYPE],
-                                               [WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN]]
+        self._ffs["typeDefinitionPart"]     = ([WrapTk.TYPE],
+                                               [WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN])
 
-        self._ffs["typeDefinition"]         = [[WrapTk.ID],
-                                               [WrapTk.ID, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN]]
+        self._ffs["typeDefinition"]         = ([WrapTk.ID],
+                                               [WrapTk.ID, WrapTk.VAR, WrapTk.PROCEDURE, WrapTk.BEGIN])
 
-        self._ffs["newType"]                = [[WrapTk.ARRAY, WrapTk.RECORD],
-                                               [WrapTk.SEMICOLON]]
+        self._ffs["newType"]                = ([WrapTk.ARRAY, WrapTk.RECORD],
+                                               [WrapTk.SEMICOLON])
 
-        self._ffs["newArrayType"]           = [[WrapTk.ARRAY],
-                                               [WrapTk.SEMICOLON]]
+        self._ffs["newArrayType"]           = ([WrapTk.ARRAY],
+                                               [WrapTk.SEMICOLON])
 
-        self._ffs["indexRange"]             = [[WrapTk.NUMERAL, WrapTk.ID],
-                                               [WrapTk.RIGHTBRACKET]]
+        self._ffs["indexRange"]             = ([WrapTk.NUMERAL, WrapTk.ID],
+                                               [WrapTk.RIGHTBRACKET])
 
-        self._ffs["newRecordType"]          = [[WrapTk.RECORD],
-                                               [WrapTk.SEMICOLON]]
+        self._ffs["newRecordType"]          = ([WrapTk.RECORD],
+                                               [WrapTk.SEMICOLON])
 
-        self._ffs["fieldList"]              = [[WrapTk.ID],
-                                               [WrapTk.END]]
+        self._ffs["fieldList"]              = ([WrapTk.ID],
+                                               [WrapTk.END])
 
-        self._ffs["recordSection"]          = [[WrapTk.ID],
-                                               [WrapTk.SEMICOLON, WrapTk.END]]
+        self._ffs["recordSection"]          = ([WrapTk.ID],
+                                               [WrapTk.SEMICOLON, WrapTk.END])
 
-        self._ffs["variableDefinitionPart"] = [[WrapTk.VAR],
-                                               [WrapTk.PROCEDURE, WrapTk.BEGIN]]
+        self._ffs["variableDefinitionPart"] = ([WrapTk.VAR],
+                                               [WrapTk.PROCEDURE, WrapTk.BEGIN])
 
-        self._ffs["variableDefinition"]     = [[WrapTk.ID],
-                                               [WrapTk.ID, WrapTk.PROCEDURE, WrapTk.BEGIN]]
+        self._ffs["variableDefinition"]     = ([WrapTk.ID],
+                                               [WrapTk.ID, WrapTk.PROCEDURE, WrapTk.BEGIN])
 
-        self._ffs["variableGroup"]          = [[WrapTk.ID],
-                                               [WrapTk.SEMICOLON, WrapTk.RIGHTPARENTHESIS]]
+        self._ffs["variableGroup"]          = ([WrapTk.ID],
+                                               [WrapTk.SEMICOLON, WrapTk.RIGHTPARENTHESIS])
 
-        self._ffs["procedureDefinition"]    = [[WrapTk.PROCEDURE],
-                                               [WrapTk.BEGIN]]
+        self._ffs["procedureDefinition"]    = ([WrapTk.PROCEDURE],
+                                               [WrapTk.BEGIN])
 
-        self._ffs["procedureBlock"]         = [[WrapTk.LEFTPARENTHESIS, WrapTk.SEMICOLON],
-                                               [WrapTk.SEMICOLON]]
+        self._ffs["procedureBlock"]         = ([WrapTk.LEFTPARENTHESIS, WrapTk.SEMICOLON],
+                                               [WrapTk.SEMICOLON])
 
-        self._ffs["formalParameterList"]    = [[WrapTk.VAR, WrapTk.ID],
-                                               [WrapTk.RIGHTPARENTHESIS]]
+        self._ffs["formalParameterList"]    = ([WrapTk.VAR, WrapTk.ID],
+                                               [WrapTk.RIGHTPARENTHESIS])
 
-        self._ffs["parameterDefinition"]    = [[WrapTk.VAR, WrapTk.ID],
-                                              [WrapTk.SEMICOLON, WrapTk.RIGHTPARENTHESIS]]
+        self._ffs["parameterDefinition"]    = ([WrapTk.VAR, WrapTk.ID],
+                                              [WrapTk.SEMICOLON, WrapTk.RIGHTPARENTHESIS])
 
-        self._ffs["statement"]              = [[WrapTk.ID, WrapTk.IF, WrapTk.WHILE, WrapTk.BEGIN],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON]]
+        self._ffs["statement"]              = ([WrapTk.ID, WrapTk.IF, WrapTk.WHILE, WrapTk.BEGIN],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON])
 
-        self._ffs["statementGroup"]         = [[WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.LEFTPARENTHESIS],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON]]
+        self._ffs["statementGroup"]         = ([WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.LEFTPARENTHESIS],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON])
 
-        self._ffs["procedureStatement"]     = [[WrapTk.LEFTPARENTHESIS],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON]]
+        self._ffs["procedureStatement"]     = ([WrapTk.LEFTPARENTHESIS],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON])
 
-        self._ffs["actualParameterList"]    = [[WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
-                                               [WrapTk.RIGHTPARENTHESIS]]
+        self._ffs["actualParameterList"]    = ([WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
+                                               [WrapTk.RIGHTPARENTHESIS])
 
-        self._ffs["ifStatement"]            = [[WrapTk.IF],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON]]
+        self._ffs["ifStatement"]            = ([WrapTk.IF],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON])
 
-        self._ffs["whileStatement"]         = [[WrapTk.WHILE],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON]]
+        self._ffs["whileStatement"]         = ([WrapTk.WHILE],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON])
 
-        self._ffs["compoundStatement"]      = [[WrapTk.BEGIN],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.PERIOD]]
+        self._ffs["compoundStatement"]      = ([WrapTk.BEGIN],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.PERIOD])
 
-        self._ffs["expression"]             = [[WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN]]
+        self._ffs["expression"]             = ([WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN])
 
-        self._ffs["relationalOperator"]     = [[WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS],
-                                               [WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS]]
+        self._ffs["relationalOperator"]     = ([WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS],
+                                               [WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS])
 
-        self._ffs["simpleExpression"]       = [[WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
-                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS]]
+        self._ffs["simpleExpression"]       = ([WrapTk.PLUS, WrapTk.MINUS, WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
+                                               [WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS])
 
-        self._ffs["signOperator"]           = [[WrapTk.PLUS, WrapTk.MINUS],
-                                               [WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS]]
+        self._ffs["signOperator"]           = ([WrapTk.PLUS, WrapTk.MINUS],
+                                               [WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS])
 
-        self._ffs["additiveOperator"]       = [[WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR],
-                                               [WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS]]
+        self._ffs["additiveOperator"]       = ([WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR],
+                                               [WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS])
 
-        self._ffs["term"]                   = [[WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
-                                               [WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS]]
+        self._ffs["term"]                   = ([WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
+                                               [WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS])
 
-        self._ffs["multiplyingOperator"]    = [[WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND],
-                                               [WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS]]
+        self._ffs["multiplyingOperator"]    = ([WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND],
+                                               [WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS])
 
-        self._ffs["factor"]                 = [[WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
-                                               [WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS]]
+        self._ffs["factor"]                 = ([WrapTk.NUMERAL, WrapTk.ID, WrapTk.NOT, WrapTk.LEFTPARENTHESIS],
+                                               [WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS])
 
-        self._ffs["selector"]               = [[WrapTk.LEFTBRACKET, WrapTk.PERIOD],
-                                               [WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS]]
+        self._ffs["selector"]               = ([WrapTk.LEFTBRACKET, WrapTk.PERIOD],
+                                               [WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS])
 
-        self._ffs["indexSelector"]          = [[WrapTk.LEFTBRACKET],
-                                               [WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS]]
+        self._ffs["indexSelector"]          = ([WrapTk.LEFTBRACKET],
+                                               [WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS])
 
-        self._ffs["fieldSelector"]          = [[WrapTk.PERIOD],
-                                               [WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS]]
+        self._ffs["fieldSelector"]          = ([WrapTk.PERIOD],
+                                               [WrapTk.LEFTBRACKET, WrapTk.PERIOD, WrapTk.BECOMES, WrapTk.ASTERISK, WrapTk.DIV, WrapTk.MOD, WrapTk.AND, WrapTk.PLUS, WrapTk.MINUS, WrapTk.OR, WrapTk.ELSE, WrapTk.END, WrapTk.SEMICOLON, WrapTk.COMMA, WrapTk.RIGHTPARENTHESIS, WrapTk.DO, WrapTk.RIGHTBRACKET, WrapTk.THEN, WrapTk.LESS, WrapTk.EQUAL, WrapTk.GREATER, WrapTk.NOTGREATER, WrapTk.NOTEQUAL, WrapTk.NOTLESS])
 
-        self._ffs["constant"]               = [[WrapTk.ID, WrapTk.NUMERAL],
-                                               [WrapTk.SEMICOLON, WrapTk.DOUBLEDOT, WrapTk.RIGHTBRACKET]]
+        self._ffs["constant"]               = ([WrapTk.ID, WrapTk.NUMERAL],
+                                               [WrapTk.SEMICOLON, WrapTk.DOUBLEDOT, WrapTk.RIGHTBRACKET])
 
 
     def first(self, noterm):
         """ Devuelve el conjunto FIRST de un simbolo no terminal de la gramatica
         """
         self._tkset = frozenset(self._ffs[noterm][0])
-        #self._f =
-        #print self._f
-        #for token in self._f[0]:
-            #self._tkset.add(token)
         return self._tkset
 
     def follow(self, noterm):
