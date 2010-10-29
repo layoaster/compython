3212a282a0e7cc827107839b2f00687d43e42fb1
s.armasperez <s.armasperez@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-29 19:00:45 +0000

    Nuevo parser.py

---
+++ p4/parser.py
@@ -0,0 +1,56 @@
+from lexan import LexAn
+from token import *
+from error import *
+#from table import *
+from stack import *
+from nonterm import *
+from production import *
+
+class SynAn:
+    """ Clase Analizador Sintactico:
+        Comprueba que la secuencia de tokens de entrada sea acorde con las
+        especificaciones de la gramatica de Pascal-
+    """
+    def __init__(self):
+        """ Constructor de la clase. Atributos:
+            _lookahead = token o simbolo de preanalisis
+            _scanner = instancia de la clase analizador lexico
+        """
+        self._scanner = None
+        self._lookahead = None
+        self._top = None
+        
+    def start(self, fin):
+        """ Comienzo del analizador sintactico. Se encarga de inicializar el lexico,
+            ordenarle abrir el fichero y recoger el primer token de entrada para comenzar
+            el analisis
+        """
+        self._scanner = LexAn()
+        try:
+            self._scanner.openFile(fin)
+        except IOError:
+            raise
+        
+        self._stack = Stack()
+#        self._table = ParsingTable()
+
+        self._stack.push(Token(WrapTk.ENDTEXT))
+        self._stack.push(NonTerm(WrapNT.PROGRAM))
+        self._stack.push(Token(WrapTk.PERIOD))
+#        self._stack.push(<Program>)
+        while self._stack.top() != Token(WrapTk.ENDTEXT):
+            self._top = self._stack.top()
+            self._lookahead = self._scanner.yyLex()
+            if isinstance(self._top, Token):    # Si en el top hay un token
+                if self._top.getToken() == self._lookahead.getToken():
+                    self._stack.pop()
+                    print "macheo", self._lookahead.getToken()
+                    self._lookahead = self._scanner.yyLex()
+                else:       # El top es diferente del lookahead
+                    print self._scanner.getPos(),
+                    print "Syntax Error:", self._top.getLexeme(), "found",
+                    print "-", self._lookahead.getLexeme(), "expected."
+                    exit(1)
+            else:                   # Si en el top hay un no terminal
+                print "futura ampliacion"
+                exit(1)
