e67d563fcdedb4ee5a8a9f54e78470cd6d94a957
lionelmena@gmail.com <lionelmena@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-29 10:49:32 +0000

    Inicio de la practica 4

---
+++ p4/lexan.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+        $Id$
+Description: Modulo del Analizador Lexico para Pascal-.
+    $Author$ Lionel Aster Mena Garcia, Alejandro Samarin Perez, Sergio Armas Perez
+      $Date$
+  $Revision$
+"""
+
+import sys
+import string
+import re
+from error import LexError
+from st import SymbolTable, st
+from token import WrapTk, Token
+
+class LexAn:
+
+    _patterns = [
+        (WrapTk.COMMENT,         r"\(\*|\{|//"),
+        (WrapTk.ASTERISK,        r"\*"),
+        (WrapTk.BECOMES,         r":="),
+        (WrapTk.COLON,           r":"),
+        (WrapTk.COMMA,           r","),
+        (WrapTk.DOUBLEDOT,       r"\.\."),
+        (WrapTk.EQUAL,           r"="),
+        (WrapTk.NOTEQUAL,        r"\<\>"),
+        (WrapTk.NOTGREATER,      r"\<="),
+        (WrapTk.NOTLESS,         r"\>="),
+        (WrapTk.GREATER,         r"\>"),
+        (WrapTk.ID,              r"[a-zA-Z]\w*"),
+        (WrapTk.LEFTBRACKET,     r"\["),
+        (WrapTk.LEFTPARENTHESIS, r"\("),
+        (WrapTk.LESS,            r"\<"),
+        (WrapTk.MINUS,           r"-"),
+        (WrapTk.NUMERAL,         r"\d+"),
+        (WrapTk.PERIOD,          r"\."),
+        (WrapTk.PLUS,            r"\+"),
+        (WrapTk.RIGHTBRACKET,    r"\]"),
+        (WrapTk.RIGHTPARENTHESIS,r"\)"),
+        (WrapTk.SEMICOLON,       r";")
+    ]
+
+    def __init__(self):
+        """ Constructor de la clase
+        """
+        self._nline = 0
+        self._ncol = 1
+        self._tokstart = 1
+        self._buffer = ""
+        self._fin = None
+        self._flags = re.UNICODE | re.IGNORECASE
+
+        parts = []
+        for name, rule in self._patterns:
+            parts.append("(?P<%s>%s)" % (WrapTk.toStr(name), rule))
+
+        self._regex = re.compile("|".join(parts), self._flags)
+        self._wsregex = re.compile("\s*", re.MULTILINE)
+
+    def openFile(self, fin):
+        try:
+            self._fin = open(fin, "rU")
+        except IOError:
+            raise
+
+    def _readLine(self):
+        """ Lee una linea del fichero y la inserta en el buffer, ignorando lineas en blanco
+        """
+        while self._buffer == "":
+            self._buffer = self._fin.readline()
+            if self._buffer == "": # EOF
+                return False
+            self._nline += 1
+            self._ncol = 1
+            self._buffer = self._buffer.rstrip()
+
+        # Ignorar espacios en blanco
+        wsmatch = self._wsregex.match(self._buffer)
+        if wsmatch:
+            self._buffer = self._buffer[wsmatch.end():]
+            self._ncol += wsmatch.end()
+            self._tokstart = self._ncol
+        return True
+
+    def _ignComment(self):
+        """ Procesado de comentarios, incluyendo comentarios anidados, multilinea y malformados (sin cerrar)
+        """
+        comment = re.compile("(?P<leftbracket>\{)|(?P<leftparenthesis>\(\*)|(?P<rightbracket>\})|(?P<rightparenthesis>\*\))|(?P<doublebar>//)")
+        match = comment.search(self._buffer)
+        if match.lastgroup != "doublebar":
+            nbra = 1
+            npar = 1
+            flag = True
+            while (nbra != 0 or npar != 0):
+                if flag:
+                    nbra = 0
+                    npar = 0
+                    flag = False
+                if match:
+                    if match.lastgroup == "leftbracket":
+                        nbra += 1
+                    if match.lastgroup == "leftparenthesis":
+                        npar += 1
+                    if match.lastgroup == "rightbracket":
+                        nbra -= 1
+                    if match.lastgroup == "rightparenthesis":
+                        npar -= 1
+
+                    self._buffer = self._buffer[match.end():]
+                    match = comment.search(self._buffer)
+                else:
+                    self._buffer = ""
+                    if self._readLine():
+                        match = comment.search(self._buffer)
+                        if match:
+                            pass #print match.lastgroup (que habia aqui Lio? creo recordar que este if no estaba simplemente pero no se)
+                    else:
+                        return False
+            if len(self._buffer) == 0:
+                self._readLine()
+            return True
+        else:
+            self._buffer = ""
+            self._readLine()
+            return True
+
+    def getPos(self):
+        return (self._nline, self._tokstart)
+
+    def yyLex(self):
+        if self._readLine():
+            match = self._regex.match(self._buffer)
+
+            if match is None:
+                self._buffer = self._buffer[1:]
+                self._ncol += 1
+                raise LexError(LexError.UNKNOWN_CHAR, self.getPos())
+
+            token = WrapTk.toToken(match.lastgroup)
+            value = match.group(match.lastgroup)
+
+            while token == WrapTk.COMMENT:
+                if self._ignComment():
+                    # Varios comentarios bien hechos separados por espacios
+                    self._readLine()
+                    match = self._regex.match(self._buffer)
+                    if match is None:
+                        return Token(WrapTk.ENDTEXT)
+                    token = WrapTk.toToken(match.lastgroup)
+                    value = match.group(match.lastgroup)
+                else:
+                    raise LexError(LexError.UNCLOSED_COM, self.getPos())
+
+            self._ncol += match.end()
+            self._buffer = self._buffer[match.end():]
+
+            if token == WrapTk.ID:
+                if st.isReserved(value.lower()):
+                    return Token(st.getIndex(value.lower()))
+                if not st.isIn(value.lower()):
+                    st.insert(value.lower())
+                return Token(WrapTk.ID, value.lower())
+            elif token == WrapTk.NUMERAL:
+                if int(value) > 32767:
+                    raise LexError(LexError.INT_OVERFLOW, self.getPos())
+                else:
+                    return Token(WrapTk.NUMERAL, int(value))
+            else:   # Reconocido token valido
+                return Token(token)
+        else:   # Se ha llegado a fin del fichero
+            return Token(WrapTk.ENDTEXT)
---
+++ p4/st.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+        $Id$
+Description: Analizador Lexico para Pascal-.
+    $Author$ Lionel Aster Mena Garcia, Alejandro Samarin Perez, Sergio Armas Perez
+      $Date$
+  $Revision$
+"""
+
+from token import WrapTk
+
+class SymbolTable:
+    """Clase para la gestion de la tabla de simbolos"""
+
+    def __init__(self):
+        """ Descripción:
+                El constructor inicializa la tabla de simbolos insertando
+                las palabras reservadas y los identificadores estandar,
+                haciendo uso de la clase Dictionary, que implementa una
+                tabla hash. La clave de busqueda es una cadena de caracteres
+                y el valor una lista de 2 elementos: 1 valor boolean que
+                indica si la cadena es una palabra reservadas y 1 indice
+                para los identificadores que se utilizara en sucesivas etapas
+                del compilador. Las palabras reservadas no tienen indice,
+                pero utilizamos ese espacio para almacenar su token_ID.
+            Parametros:
+                Ninguno
+
+            Valor de retorno:
+                Ninguno
+	"""
+        self._table = {}
+        self._index = 1
+        # Insertamos las palabras reservadas
+        self._table["and"] = [True, WrapTk.AND]
+        self._table["array"] = [True, WrapTk.ARRAY]
+        self._table["begin"] = [True, WrapTk.BEGIN]
+        self._table["const"] = [True, WrapTk.CONST]
+        self._table["div"] = [True, WrapTk.DIV]
+        self._table["do"] = [True, WrapTk.DO]
+        self._table["else"] = [True, WrapTk.ELSE]
+        self._table["end"] = [True, WrapTk.END]
+        self._table["if"] = [True, WrapTk.IF]
+        self._table["mod"] = [True, WrapTk.MOD]
+        self._table["not"] = [True, WrapTk.NOT]
+        self._table["of"] = [True, WrapTk.OF]
+        self._table["or"] = [True, WrapTk.OR]
+        self._table["procedure"] = [True, WrapTk.PROCEDURE]
+        self._table["program"] = [True, WrapTk.PROGRAM]
+        self._table["record"] = [True, WrapTk.RECORD]
+        self._table["then"] = [True, WrapTk.THEN]
+        self._table["type"] = [True, WrapTk.TYPE]
+        self._table["var"] = [True, WrapTk.VAR]
+        self._table["while"] = [True, WrapTk.WHILE]
+        # Insertamos los identificadores estandar
+        self.insert("integer")
+        self.insert("boolean")
+        self.insert("false")
+        self.insert("true")
+        self.insert("read")
+        self.insert("write")
+
+    def insert(self, lex, reserved = False):
+        """ Descripción:
+                Inserta los identificadores en la tabla de simbolos que
+                detecta el analizador lexico. Al no ser palabras reservadas,
+                por defecto se toma False para el primer elemento del valor.
+                Como indice se utiliza el valor actual de self._index, el cual
+                se incrementa para dejarlo preparado para la siguiente entrada.
+
+            Parametros:
+                - lex: Cadena de caracteres que se inserta como identificador.
+                - reserved: Boolean que indica si el identificador es una
+                            palabra reservada o no. Por defecto, False.
+
+            Valor de retorno:
+                Ninguno
+        """
+        atributes = [reserved, self._index]
+        self._table[lex] = atributes
+        self._index += 1
+
+    def isIn(self, lex):
+        """ Descripción:
+                Comprueba si una cadena de caracteres esta en la tabla.
+
+            Parametros:
+                - lex: Cadena de caracteres que se buscara.
+
+            Valor de retorno:
+                True si se encontro; False en caso contrario.
+        """
+        return lex in self._table
+
+    def isReserved(self, lex):
+        """ Descripción:
+                Comprueba si una cadena de caracteres es palabra reservada.
+
+            Parametros:
+                - lex: Cadena de caracteres que se buscara.
+
+            Valor de retorno:
+                True si es palabra reservada; False en caso contrario.
+        """
+        try:
+            return self._table[lex][0]
+        except KeyError:
+            return None
+
+    def getIndex(self, lex):
+        """ Descripción:
+                Devuelve el indice asociado a una cadena de caracteres.
+            Parametros:
+                - lex: Cadena de caracteres que se buscara.
+
+            Valor de retorno:
+                Indice asociado a la cadena de caracteres.
+        """
+        try:
+            return self._table[lex][1]
+        except KeyError:
+            return None
+        except IndexError:
+            return None
+
+    def printTable(self):
+        """ Descripción:
+                Imprime la tabla de simbolos por pantalla a efectos de
+                comprobacion. No forma parte del proceso de compilacion.
+.
+            Parametros:
+                Ninguno.
+
+            Valor de retorno:
+                Nada.
+        """
+
+        for i in self._table:
+            print i + "\t\t",
+            for j in self._table[i]:
+                print j, "\t",
+            print ""
+
+# Objeto de la clase SymbolTable que se utilizara como tabla de simbolos
+# del compilador. El hecho de que se instancie en este modulo obedece a
+# que debe estar accesible durante todo el proceso de compilacion, para
+# que otras fases accedan y/o escriban en ella.
+st = SymbolTable()
