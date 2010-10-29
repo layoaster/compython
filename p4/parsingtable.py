888d61ed35ea4e76573729c4e7a291aad43b2e0f
lionelmena@gmail.com <lionelmena@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-10-29 23:54:12 +0000

    reubicando la carpeta grammar a la raiz del trunk y hardcodeado de la tabla completo hasta  procedureDefinition

--- p4/parsingtable.py
+++ p4/parsingtable.py
@@ -58,7 +58,120 @@ class ParsingTable:
                                                                                        NonTerm(WrapNT.CONSTDEF),
                                                                                        NonTerm(WrapNT.CONSTDEF2)]),
 
-                                                     Token(WrapTk.TYPE) : None}
+                                                     Token(WrapTk.TYPE) : None,
+                                                     Token(WrapTk.VAR) : None,
+                                                     Token(WrapTk.PROCEDURE) : None,
+                                                     Token(WrapTk.BEGIN) : None}
+
+        self._table[NonTerm(WrapNT.CONSTDEF)] = {Token(WrapTk.ID) : Production([Token(WrapTk.ID),
+                                                                                Token(WrapTk.EQUAL),
+                                                                                NonTerm(WrapNT.CONSTANT),
+                                                                                Token(WrapTk.SEMICOLON)])}
+
+        self._table[NonTerm(WrapNT.CONSTDEF2)] = {Token(WrapTk.ID) : Production([NonTerm(WrapNT.CONSTDEF),
+                                                                                 NonTerm(WrapNT.CONSTDEF2)]),
+
+                                                  Token(WrapTk.TYPE) : None,
+                                                  Token(WrapTk.VAR) : None,
+                                                  Token(WrapTk.PROCEDURE) : None,
+                                                  Token(WrapTk.BEGIN) : None}
+
+        self._table[NonTerm(WrapNT.TYPEDEFPART)] = {Token(WrapTk.TYPE) : Production([Token(WrapTk.TYPE),
+                                                                                     NonTerm(WrapNT.TYPEDEF),
+                                                                                     NonTerm(WrapNT.TYPEDEF2)]),
+
+                                                    Token(WrapTk.VAR) : None,
+                                                    Token(WrapTk.PROCEDURE) : None,
+                                                    Token(WrapTk.BEGIN) : None}
+
+        self._table[NonTerm(WrapNT.TYPEDEF)] = {Token(WrapTk.ID) : Production([Token(WrapTk.ID),
+                                                                               Token(WrapTk.EQUAL),
+                                                                               NonTerm(WrapNT.NEWTYPE),
+                                                                               Token(WrapTk.SEMICOLON)])}
+
+        self._table[NonTerm(WrapNT.TYPEDEF2)] = {Token(WrapTk.ID) : Production([NonTerm(WrapNT.TYPEDEF),
+                                                                                NonTerm(WrapNT.TYPEDEF2)]),
+
+                                                 Token(WrapTk.VAR) : None,
+                                                 Token(WrapTk.PROCEDURE) : None,
+                                                 Token(WrapTk.BEGIN) : None}
+
+        self._table[NonTerm(WrapNT.NEWTYPE)] = {Token(WrapTk.ARRAY) : Production([NonTerm(WrapNT.NEWARRAYTYPE)]),
+
+                                                 Token(WrapTk.RECORD) : Production([NonTerm(WrapNT.NEWRECTYPE)])}
+
+        self._table[NonTerm(WrapNT.NEWARRAYTYPE)] = {Token(WrapTk.ARRAY) : Production([Token(WrapTk.ARRAY),
+                                                                                       Token(WrapTk.LEFTBRACKET),
+                                                                                       NonTerm(WrapNT.INDEXRANGE),
+                                                                                       Token(WrapTk.RIGHTBRACKET),
+                                                                                       Token(WrapTk.OF),
+                                                                                       Token(WrapTk.ID)])}
+
+        self._table[NonTerm(WrapNT.INDEXRANGE)] = {Token(WrapTk.NUMERAL) : Production([NonTerm(WrapNT.CONSTANT),
+                                                                                       Token(WrapTk.DOUBLEDOT),
+                                                                                       NonTerm(WrapNT.CONSTANT)]),
+
+                                                    Token(WrapTk.ID) : Production([NonTerm(WrapNT.CONSTANT),
+                                                                                   Token(WrapTk.DOUBLEDOT),
+                                                                                   NonTerm(WrapNT.CONSTANT)])}
+
+        self._table[NonTerm(WrapNT.NEWRECTYPE)] = {Token(WrapTk.RECORD) : Production([Token(WrapTk.RECORD),
+                                                                                      NonTerm(WrapNT.RECSECTION),
+                                                                                      NonTerm(WrapNT.FIELDLIST),
+                                                                                      Token(WrapTk.END)])}
+
+        self._table[NonTerm(WrapNT.FIELDLIST)] = {Token(WrapTk.ID) : Production([Token(WrapTk.SEMICOLON),
+                                                                                 NonTerm(WrapNT.RECSECTION),
+                                                                                 NonTerm(WrapNT.FIELDLIST)])}
+
+        self._table[NonTerm(WrapNT.FIELDLIST2)] = {Token(WrapTk.SEMICOLON) : Production([Token(WrapTk.SEMICOLON),
+                                                                                         NonTerm(WrapNT.RECSECTION),
+                                                                                         NonTerm(WrapNT.FIELDLIST)]),
+
+                                                   Token(WrapTk.END) : None}
+
+        self._table[NonTerm(WrapNT.RECSECTION)] = {Token(WrapTk.ID) : Production([Token(WrapTk.ID),
+                                                                                  NonTerm(WrapNT.RECSECTION2),
+                                                                                  Token(WrapTk.COLON),
+                                                                                  Token(WrapTk.ID)])}
+
+        self._table[NonTerm(WrapNT.RECSECTION2)] = {Token(WrapTk.COMMA) : Production([Token(WrapTk.COMMA),
+                                                                                      NonTerm(WrapNT.RECSECTION2)]),
+
+                                                   Token(WrapTk.COLON) : None}
+
+        self._table[NonTerm(WrapNT.VARDEFPART)] = {Token(WrapTk.VAR) : Production([Token(WrapTk.VAR),
+                                                                                   NonTerm(WrapNT.VARDEF),
+                                                                                   NonTerm(WrapNT.VARDEF2)]),
+
+                                                   Token(WrapTk.PROCEDURE) : None,
+                                                   Token(WrapTk.BEGIN) : None}
+
+        self._table[NonTerm(WrapNT.VARDEF)] = {Token(WrapTk.ID) : Production([NonTerm(WrapNT.RECSECTION),
+                                                                              Token(WrapTk.SEMICOLON)])}
+
+        self._table[NonTerm(WrapNT.VARDEF2)] = {Token(WrapTk.ID) : Production([NonTerm(WrapNT.VARDEF),
+                                                                               NonTerm(WrapNT.VARDEF2)]),
+
+                                                Token(WrapTk.PROCEDURE) : None,
+                                                Token(WrapTk.BEGIN) : None}
+
+        self._table[NonTerm(WrapNT.VARGROUP)] = {Token(WrapTk.ID) : Production([Token(WrapTk.ID),
+                                                                                NonTerm(WrapNT.VARGROUP2),
+                                                                                Token(WrapTk.COLON),
+                                                                                Token(WrapTk.ID)])}
+
+        self._table[NonTerm(WrapNT.VARGROUP2)] = {Token(WrapTk.COMMA) : Production([Token(WrapTk.COMMA),
+                                                                                    NonTerm(WrapNT.VARGROUP2)]),
+
+                                                  Token(WrapTk.COLON) : None}
+
+        self._table[NonTerm(WrapNT.PROCDEF)] = {Token(WrapTk.PROCEDURE) : Production([Token(WrapTk.PROCEDURE),
+                                                                                      Token(WrapTk.ID),
+                                                                                      NonTerm(WrapNT.PROCBLOCK),
+                                                                                      Token(WrapTk.SEMICOLON),
+                                                                                      NonTerm(WrapNT.PROCDEF)]),
+                                                Token(WrapTk.BEGIN) : None}
 
     def getCell(self, nterm, tok):
         row = self._table.get(nterm)
