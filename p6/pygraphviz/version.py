a7371e17bb8b75628af5de68358d0b46086ce942
alejandrosamarin@gmail.com <alejandrosamarin@gmail.com@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-11-22 13:03:13 +0000

    Incluido modulo pygraphviz

---
+++ p6/pygraphviz/version.py
@@ -0,0 +1,11 @@
+"""
+Version information for PyGraphviz, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__version__ = '1.0'
+__revision__ = None
+__date__ = 'Sun Aug  1 07:58:06 2010'
+
