103eb4a02fc6eda0dcebd2bdb00f4b88b61f0476
s.armasperez <s.armasperez@fc510d7b-04d7-fcf3-d06f-2a55c07aba96>
2010-12-14 17:17:59 +0000

    subiendo maxpetadas.pas...

---
+++ p8/test/maxpetadas.pas
@@ -0,0 +1,54 @@
+program prueba(input);
+
+const a = 1;
+      b = 10;
+
+type T = array[a..b] of integer;
+     S = array[1..10] of T;
+     Z = record
+            f : T;
+            g : S;
+         end;
+     V = array[1..10] of Z;
+     W = record
+            f : V;
+         end;
+
+var x : T;
+    i : Z;
+    j : V;
+    k : W;
+
+    procedure funcion(var prueba, funcion, integer : boolean; a, boolean : integer);
+
+	type S = record
+                   f : T;
+                   g : write;
+	end;
+     {T = array[a..10] of NoName;}
+
+	var x : T;
+    	    y : S;
+    	    YesName : boolean;
+
+	begin
+   	YesName := true;
+   	{read(y.f[5]);
+   	y.g := read(y.f[5])}
+   	x[4] := YesName
+	end;
+
+
+
+begin
+    funcion(integer, x, do, b, prueba);
+    x[9999] := false;
+    {m[4] := 45;}
+    i[4] := 45;
+    {i.g[9999][9999] := -0;}
+    {i.f[-1] := 9999;
+    k.g[][-0] := 0;
+    k.f[].g[1][1] := 43;}
+    k.f[1].f[1] := 12;
+{    k.f[1].g[0][0] := v.f[1].f[1]}
+end.
